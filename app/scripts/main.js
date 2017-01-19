55d5da47400cbd6540122bff515bfa9b86df3b5f
Mike G <mkgalligar@gmail.com>
2017-01-19 09:29:50 -0800

    fix grunt build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+$( document ).ready(function() {
+    var heights = $(".well").map(function() {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+    $(".well").height(maxHeight);
+});
