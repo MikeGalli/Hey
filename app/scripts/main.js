a9563f5b296e15397c7f7ba45af74f9072ea04ac
Mike G <mkgalligar@gmail.com>
2017-01-27 16:03:56 -0800

    increase responsiveness

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,1 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-
-
-$( document ).ready(function() {
-    var heights = $("container-fluid").map(function() {
-        return $(this).height();
-    }).get(),
-
-    maxHeight = Math.max.apply(null, heights);
-
-    $("container-fluid").height(maxHeight);
-});
